889356895bd06c612c6111f5748b9355ce2985aa
billy3321 <billy3321@gmail.com>
2009-02-28 13:18:52 +0800

    fix some bugs on zenity...

--- Localization/cin.py
+++ Localization/cin.py
@@ -140,20 +140,22 @@ def user_scope (selected_cin):
 
 
 def main():
-    zypper_cmd = "zypper install "
-
+    zypper_cmd = "zypper install -l --yes "
+    xhost_cmd = "xhost local:%s" % (os.environ['REAL_USER'])
+    print xhost_cmd
+    os.system(xhost_cmd)
     gcin_text = "gcin \"由台灣網友開發的輸入法，有許多在地化的調校，\n是在臺灣相當受到歡迎的中文輸入法。\n有許多方便的功能，穩定而強大，內建多種輸入法，\n包括功能類似微軟新注音的詞音輸入法、並「可支援無蝦米」。\n但和 Windows 下常見的操作習慣相差不少，新手可能會很不習慣。\n\""
     scim_text = "scim \"Ubuntu 預設的中文輸入法，可以輸入多國文字，功能強大，\n操作和 Windows 上接近，內含類似新注音，相當知名的新酷音輸入法，\n但是比較龐大，目前穩定性也不及 gcin\n\""
     dialog_text = "--text=下列是最常見的二種中文輸入法，'請選擇您喜好的輸入法，\n若您無法決擇，建議可以考慮 gcin：\n'"
     zenity_cmd = Template("zenity --width=512 --height=480 --list --title='選擇輸入法' --column='名稱' --column='敘述' $dialog_text $gcin $scim").substitute(gcin=gcin_text, scim=scim_text, dialog_text=dialog_text)
-
+    print zenity_cmd
     fin, fout = os.popen2(zenity_cmd)
     selected_cin = fout.read().strip()
 
     #os.system(apt_cmd + "im-switch")
 
     if selected_cin == 'gcin' or selected_cin == 'scim':
-        os.system(apt_cmd + selected_cin)
+        os.system(zypper_cmd + selected_cin)
 
         if selected_cin == "scim":
             os.system(zypper_cmd + "scim-qtimm scim-chewing")
