f1fb5b1448fa528739b4d027e0e12cc6ee5b751b
mrmoneyc <moneyc.net@gmail.com>
2009-04-20 09:47:16 +0800

    Port scripts to openSUSE (Networking)

---
+++ Networking/amule.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>
+# Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>
+# Copyright (C) 2009 張君平 Chun-ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>
+# Last Modified: 20 Apr 2009
+# Released under GNU General Public License
+# Please run as root.
+#
+# @name_zhTW '安裝 aMule P2P 軟體 - 在 Linux 下養騾子'
+# @desc_zhTW 'aMule 是由 Windows 下 eMule 移植而來的跨平台軟體。'
+# @category 'Networking'
+# @maintaner '張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>'
+# @author '2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>'
+# @license 'GPL'
+# @ubuntu 
+# @debian
+# @platform 'i386 amd64'
+
+ zypper -n install amule
+
---
+++ Networking/liferea.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>
+# Copyright (C) 2008 林哲瑋 Zhe-Wei Lin (billy3321,雨蒼) <billy3321 -AT- gmail.com>
+# Copyright (C) 2009 張君平 Chun-ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>
+# Last Modified: 20 Apr 2009
+# Released under GNU General Public License
+# Please run as root.
+#
+# @name_zhTW '安裝 Liferea - 閱讀 RSS Feed 的利器'
+# @desc_zhTW 'Linux 下簡單易用的 RSS Feed 閱讀程式'
+# @category 'Networking'
+# @maintaner '張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>'
+# @author '2007 洪任諭(PCMan) <pcman.tw -AT- gmail.com>'
+# @license 'GPL'
+# @openSUSE
+# @platform 'i386 amd64'
+
+zypper -n install liferea
+
