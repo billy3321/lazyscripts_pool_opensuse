3f35235a9c4cf5b7fa1fa6731c3636738c1938b4
mrmoneyc <moneyc.net@gmail.com>
2009-11-14 20:22:59 +0800

    add useful nautilus plugin

---
+++ Customize/nautilus.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>
+# Last Modified: 27 Feb 2009
+# Released under GNU General Public License
+# Download and install madedit for i686 and x86_64 ubuntu
+# Please run as root.
+#
+# @name_enUS '' 
+# @name_zhTW '安裝數個 nautilus 外掛'
+# @desc_enUS ''
+# @desc_zhTW ''
+# @category ''
+# @maintaner '張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>'
+# @author '張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>'
+# @license 'GPL'
+# @opensuse
+# @platform 'i386 AMD64'
+
+echo
+echo '[1;33;41m 安裝 nautilus 外掛... [m'
+echo 
+
+zypper -n install nautilus-cd-burner nautilus-devel nautilus-open-terminal nautilus-open-terminal-lang nautilus-search-tool nautilus-search-tool-lang
+
+#END
