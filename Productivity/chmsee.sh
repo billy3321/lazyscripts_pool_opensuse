0a36b9ceaf46e72d6f1ffe1734793da6617fd240
mrmoneyc <moneyc.net@gmail.com>
2009-11-16 19:18:16 +0800

    add ChmSee

---
+++ Productivity/chmsee.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>
+# Last Modified: 27 Feb 2009
+# Released under GNU General Public License
+# Please run as root.
+#
+# @name_enUS 'Install ChmSee' 
+# @name_zhTW '安裝 ChmSee'
+# @desc_enUS ''
+# @desc_zhTW 'ChmSee 是一套可用來開啟 chm 檔案的工具'
+# @category 'Productivity'
+# @maintaner '張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>'
+# @author '張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>'
+# @license 'GPL'
+# @opensuse
+# @platform 'i386 AMD64'
+# @child 'Common/add-zypper-sources'
+
+echo
+echo '[1;33;41m 安裝 ChmSee... [m'
+echo 
+
+source add-zypper-sources
+add_gnome_factory
+
+zypper -n install chmsee
+
+#END
