9049694b5e1a44d8c62402a776e19b95421c8d5b
mrmoneyc <moneyc.net@gmail.com>
2009-11-17 07:55:41 +0800

    add gtkPod

---
+++ Multimedia/gtkpod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009 張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>
+# Last Modified: 27 Feb 2009
+# Released under GNU General Public License
+# Please run as root.
+#
+# @name_enUS 'Install gtkPod' 
+# @name_zhTW '安裝 gtkPod'
+# @desc_enUS 'gtkpod is a platform independent Graphical User Interface for Apple's iPod using GTK2.'
+# @desc_zhTW 'gtkPod 是一套 Linux 常用的 iPod 管理程式。'
+# @category 'Multimedia'
+# @maintaner '張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>'
+# @author '張君平 Chun-Ping Chang (mrmoneyc) <moneyc.net -AT- gmail.com>'
+# @license 'GPL'
+# @opensuse
+# @platform 'i386 AMD64'
+
+echo
+echo '[1;33;41m 安裝 gtkPod... [m'
+echo 
+
+zypper -n install gtkpod
+
+#END
